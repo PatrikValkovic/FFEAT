9f5346047a88942ccb88293b167087fa37a7c54b
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2021-03-16 16:04:58 +0100

    Add work in progress setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 
 from setuptools import setup
 
-v = '1.0.0'
+v = '0.0.0'
 # read the contents of your README file
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
@@ -20,13 +20,11 @@ setup(
     description="Framework for Evolutionary Algorithms in Torch",
     url="https://github.com/PatrikValkovic/FEAT",
     download_url='https://github.com/PatrikValkovic/FEAT/archive/v' + v + '.tar.gz',
-    long_description=README,
+    long_description="# WORK IN PROGRESS",
     long_description_content_type="text/markdown",
     author="Patrik Valkovic",
     license="GNU LGPLv3",
     packages=[
-        "ffeat",
-        "ffeat/flow"
     ],
     install_requires=[
         "numpy",
