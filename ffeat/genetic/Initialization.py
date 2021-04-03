6577536a5a2ed205acb88bf8875d714cbd6d1cf1
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2021-04-03 13:36:54 +0200

    Allow to specify type for GA initialization

--- ffeat/genetic/Initialization.py
+++ ffeat/genetic/Initialization.py
@@ -13,13 +13,14 @@ from ffeat._common.initialization.Uniform import UniformInit
 class _GAUniform(UniformInit):
     def __init__(self,
                  population_size: int,
-                dimension: int,
-                device: t.device = None):
-        super().__init__(population_size, 0, 2, dimension, t.int8, device)
+                 dimension: int,
+                 device: t.device = None,
+                 dtype=t.bool):
+        super().__init__(population_size, 0, 2, dimension, dtype, device)
 
     def __call__(self, **kargs) -> Tuple[Tuple[Any], Dict[str, Any]]:
         (r,), kargs = super().__call__(**kargs)
-        r = r.type(t.bool)
+        r = r.type(self.dtype)
         return (r,), kargs
 
 
